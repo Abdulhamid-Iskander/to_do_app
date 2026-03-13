f9cc99160ae2be500635cec646a1a56227b67d25
caesar <abdulhamidiskander@gmail.com>
2026-03-13 12:18:10 +0200

    feat: update main.dart to improve Firebase initialization and enhance app routing with language support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:to_do_app/cubit/task_cubit/task_cubit.dart';
 import 'package:to_do_app/screens/change_password_screen.dart';
 import 'package:to_do_app/screens/home_screen.dart';
@@ -7,14 +9,12 @@ import 'package:to_do_app/screens/login_screen.dart';
 import 'package:to_do_app/screens/profile_screen.dart';
 import 'package:to_do_app/screens/signup_screen.dart';
 import 'cubit/auth/auth_cubit.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'cubit/auth/auth_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
-  );
+  
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
@@ -27,17 +27,37 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => AuthCubit()),
-        BlocProvider(create: (context) => TasksCubit()), 
+        BlocProvider(create: (context) => TasksCubit()),
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: '/login',
-        routes: {
-          '/login': (context) => const LoginScreen(),
-          '/signup': (context) => const SignUpScreen(),
-          '/home': (context) => const HomeScreen(),
-          '/change-password': (context) => const ChangePasswordScreen(),
-          '/profile': (context) => const ProfileScreen(),
+      child: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return const HomeScreen();
+                }
+                return const LoginScreen();
+              },
+            ),
+            builder: (context, child) {
+              return Directionality(
+                textDirection: state.language == 'Arabic'
+                    ? TextDirection.rtl
+                    : TextDirection.ltr,
+                child: child!,
+              );
+            },
+            routes: {
+              '/login': (context) => const LoginScreen(),
+              '/signup': (context) => const SignUpScreen(),
+              '/home': (context) => const HomeScreen(),
+              '/change-password': (context) => const ChangePasswordScreen(),
+              '/profile': (context) => const ProfileScreen(),
+            },
+          );
         },
       ),
     );
