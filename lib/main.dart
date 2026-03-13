1c9b92a165df27ef0a4e01c8f7cfa9652d3d1dba
caesar <abdulhamidiskander@gmail.com>
2026-03-13 11:58:19 +0200

    chore: remove unused imports from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:to_do_app/cubit/task_cubit/task_cubit.dart';
-import 'package:to_do_app/firebase_options.dart';
 import 'package:to_do_app/screens/change_password_screen.dart';
 import 'package:to_do_app/screens/home_screen.dart';
 import 'package:to_do_app/screens/login_screen.dart';
 import 'package:to_do_app/screens/profile_screen.dart';
 import 'package:to_do_app/screens/signup_screen.dart';
 import 'cubit/auth/auth_cubit.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
